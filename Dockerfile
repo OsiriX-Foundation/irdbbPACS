29ff1d90f3d413729d78da275c3d041cca7cbf50
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-02-18 15:01:12 +0100

    Fix error

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM nginx:1.14-alpine
 
+COPY nginx.conf /etc/nginx/nginx.conf
+
 COPY script.sh /script.sh
 RUN chmod +x script.sh
 
--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 KHEOPS_TOKEN=$(cat /run/secrets/kheops_token)
 sed -i "s|\${kheops_token}|$KHEOPS_TOKEN|g" /etc/nginx/nginx.conf
 
