7e5814ce610ba10ae0dbe73ac5f969f05cfd8a00
Joël Spaltenstein <spalte@naturalimage.ch>
2020-05-13 15:45:56 +0200

    used KHEOPS_TOKEN_FILE

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-KHEOPS_TOKEN=$(cat /run/secrets/kheops_token)
+KHEOPS_TOKEN=$(cat ${KHEOPS_TOKEN_FILE:?KHEOPS_TOKEN_FILE has not been set})
 sed -i "s|\${kheops_token}|$KHEOPS_TOKEN|g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
